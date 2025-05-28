cb8445c91ae49af16f039ce7241a88789073aaca
denusbtw <denisrozum@gmail.com>
2025-05-29 02:13:32 +0300

    deploy?

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,6 +1,12 @@
+import os
+
 from .base import *
 
 env.read_env(str(BASE_DIR / ".env.production"))
+
+ALLOWED_HOSTS = ["localhost"]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
